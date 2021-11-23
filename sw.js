c2235a2aa170c5c9e7e61b13d2a6d24736b12880
Billy <kerimleyya25>
2021-11-23 11:46:09 +0100

    dq

--- sw.js
+++ sw.js
@@ -1,71 +1,95 @@
+"use strict";
 
+/** 
+    Source: https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
+*/
 
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Installation');
-    e.waitUntil(
-      caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Mise en cache globale: app shell et contenu');
-        return cache.addAll(contentToCache);
-      })
-    );
-});
+// variable definitions 
+var CACHE_NAME = 'ginkobusPWA';
 
-
-
-var contentToCache = appShellFiles
-var cacheName = 'ginkobus-v1';
-var appShellFiles = [
-  '',
-  'index.html',
-  'app.js',
-  'style.css',
-  'favicon.ico',
-  'icons/icon-32.png',
-  'icons/icon-64.png',
-  'icons/icon-96.png',
-  'icons/icon-128.png',
-  'icons/icon-168.png',
-  'icons/icon-192.png',
-  'icons/icon-256.png',
-  'icons/maskable_icon.png',
-  'icons/icon-512.png'
+var contentToCache = [
+  './index.html',    
+  './manifest.json',    
+  './style.css', 
+  './app.js', 
+  './icons/favicon.ico',
+  './icons/icon-32.png',
+  './icons/icon-64.png',
+  './icons/icon-96.png',
+  './icons/icon-128.png',
+  './icons/icon-168.png',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-256.png',
+  './icons/icon-512.png',
+  './icons/maskable_icon.png'
 ];
 
-self.addEventListener('fetch', (e) => {
-    console.log('[Service Worker] Ressource récupérée '+e.request.url);
+// service worker installation
+self.addEventListener('install', function(e) {
+    console.log('[Service Worker] Install');
+    e.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+        console.log('[Service Worker] Caching application content & data');
+        return cache.addAll(contentToCache);
+    }));
 });
 
+
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((r) => {
-            console.log('[Service Worker] Récupération de la ressource: '+e.request.url);
-        return r || fetch(e.request).then((response) => {
-                  return caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Mise en cache de la nouvelle ressource: '+e.request.url);
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        });
-      })
+   
+    // Stratégie initiale : cache ou network avec mise en cache (le "false &&" empêche son application) 
+    false && e.respondWith(
+        caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+            return r || 
+                fetch(e.request).then((response) => {
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        console.log('[Service Worker] Caching new resource: '+e.request.url);
+                        cache.put(e.request, response.clone());
+                        return response;
+                    });
+                });
+        })
     );
-  });
-  contentToCache.push('icons/icon-32.png');
-  self.addEventListener('install', (e) => {
+    
+    
+    // Stratégie cache-only
+    if (contentToCache.some(file => e.request.url.endsWith(file.substr(2)) && !e.request.url.endsWith("app.js"))) {
+        console.log('[Service Worker] Loading from cache: '+e.request.url);
+        e.respondWith(caches.match(e.request));
+    }
+    else {
+        // Stratégie network + mise en cache, ou alors cache, ou réponse par défaut  
+        e.respondWith(fetch(e.request)
+            .then((response) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    console.log('[Service Worker] Fetching from network and caching resource: '+e.request.url);
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            })
+            .catch(function() { 
+                return caches.match(e.request).then((r) => {
+                    console.log('[Service Worker] Looking for resource in cache: '+e.request.url);
+                    return r; // || new Response(JSON.stringify({ error: 1 }), { headers: { 'Content-Type': 'application/json' } }); <-- si on veut renvoyer un JSON indiquant l'erreur au lieu de laisser une erreur d'accès être capturée par l'application. 
+                })
+            })
+        );
+    }
+    
+});
+
+
+self.addEventListener('activate', (e) => {
     e.waitUntil(
-      caches.open('js13kPWA-v2').then((cache) => {
-        return cache.addAll(contentToCache);
-      })
-    );
-  });
-  self.addEventListener('activate', (e) => {
-    e.waitUntil(
-      caches.keys().then((keyList) => {
+        // cleaning previous caches
+        caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
-          if(cacheName.indexOf(key) === -1) {
-            return caches.delete(key);
-          }
-        }));
-      })
+                if(CACHE_NAME.indexOf(key) === -1) {
+                    console.log("[Service Worker] Cleaning old cache");
+                    return caches.delete(key);
+                }
+          }));
+        })
     );
-  });
-  
+});
